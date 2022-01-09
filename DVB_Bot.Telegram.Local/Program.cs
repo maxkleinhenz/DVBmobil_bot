2aa817c45a6746f17d7ec0c9b970010e70533717
maxkleinhenz <max.github@outlook.de>
2022-01-09 20:16:48 +0100

    feat: add local bot project

---
+++ DVB_Bot.Telegram.Local/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using DVB_Bot.Telegram.Core;
+using DVB_Bot.Telegram.Local.Repository;
+using Microsoft.Extensions.Configuration;
+
+namespace DVB_Bot.Telegram.Local
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSecrets.json", optional: false, reloadOnChange: true);
+            var configuration = builder.Build();
+            var telegramBotToken = configuration.GetSection("telegram:token").Value;
+
+            var stopRepository = new LocalStopRepository();
+            var favoriteStopRepository = new LocalFavoriteStopRepository();
+
+            var dvbBot = new DvbTelegramBot(telegramBotToken, stopRepository, favoriteStopRepository);
+            dvbBot.TelegramBotClient.OnMessage += async (sender, eventArgs) =>
+            {
+                await dvbBot.ComputeMessageAsync(eventArgs.Message);
+            };
+
+            dvbBot.TelegramBotClient.OnCallbackQuery += async (sender, eventArgs) =>
+            {
+                await dvbBot.ComputeCallbackQueryAsync(eventArgs.CallbackQuery);
+            };
+
+            dvbBot.TelegramBotClient.StartReceiving();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            dvbBot.TelegramBotClient.StopReceiving();
+        }
+    }
+}
---
+++ DVB_Bot.Telegram.Local/Repository/LocalFavoriteStopRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DVB_Bot.Shared.Model;
+using DVB_Bot.Shared.Repository;
+
+namespace DVB_Bot.Telegram.Local.Repository
+{
+    public class LocalFavoriteStopRepository : IFavoriteStopRepository
+    {
+        private readonly List<IFavoriteStop> _favStops = new List<IFavoriteStop>();
+
+        public Task<IFavoriteStop> AddFavoriteStopAsync(string chatId, string stopShortName)
+        {
+            var favStop = new FavoriteStop(chatId, stopShortName);
+            if (!_favStops.Any(f => f.ChatId == chatId && f.StopShortName == stopShortName))
+            {
+                _favStops.Add(favStop);
+            }
+
+            return Task.FromResult((IFavoriteStop)favStop);
+        }
+
+        public Task<IFavoriteStop> RemoveFavoriteStopAsync(string chatId, string stopShortName)
+        {
+            var favStop = _favStops.SingleOrDefault(f => f.ChatId == chatId && f.StopShortName == stopShortName);
+
+            if (favStop == null)
+                return Task.FromResult((IFavoriteStop)new FavoriteStop(chatId, stopShortName));
+
+            _favStops.Remove(favStop);
+            return Task.FromResult(favStop);
+        }
+
+        public Task<List<IFavoriteStop>> GetFavoriteStopsAsync(string chatId)
+        {
+            return Task.FromResult(_favStops.Where(f => f.ChatId == chatId).ToList());
+        }
+    }
+}
