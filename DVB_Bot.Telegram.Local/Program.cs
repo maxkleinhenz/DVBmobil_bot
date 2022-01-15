2fd1022b3f0218debdb35d8f2ea774e93b806eab
maxkleinhenz <max.github@outlook.de>
2022-01-15 22:16:28 +0100

    refactor local telegram bot

--- DVB_Bot.Telegram.Local/Program.cs
+++ DVB_Bot.Telegram.Local/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,7 +15,7 @@ namespace DVB_Bot.Telegram.Local
 {
     internal class Program
     {
-        private static DvbTelegramBot dvbBot;
+        private static DvbTelegramBot _dvbBot;
 
         private static void Main(string[] args)
         {
@@ -27,11 +28,11 @@ namespace DVB_Bot.Telegram.Local
             var stopRepository = new LocalStopRepository();
             var favoriteStopRepository = new LocalFavoriteStopRepository();
 
-            dvbBot = new DvbTelegramBot(telegramBotToken, stopRepository, favoriteStopRepository);
+            _dvbBot = new DvbTelegramBot(telegramBotToken, stopRepository, favoriteStopRepository);
             
             using var cts = new CancellationTokenSource();
             var receiverOptions = new ReceiverOptions() { AllowedUpdates = { } };
-            dvbBot.TelegramBotClient.StartReceiving(UpdateHandler, ErrorHandler,
+            _dvbBot.TelegramBotClient.StartReceiving(UpdateHandler, ErrorHandler,
                 receiverOptions,
                 cts.Token);
 
@@ -41,17 +42,18 @@ namespace DVB_Bot.Telegram.Local
             cts.Cancel();
         }
 
-        private static Task ErrorHandler(ITelegramBotClient botClient, Exception arg2, CancellationToken cancellationToken)
+        private static Task<int> ErrorHandler(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            Debug.WriteLine(exception);
+            return Task.FromResult(-1);
         }
 
         private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             var handler = update.Type switch
             {
-                UpdateType.Message => dvbBot.ComputeMessageAsync(update.Message),
-                UpdateType.CallbackQuery => dvbBot.ComputeCallbackQueryAsync(update.CallbackQuery),
+                UpdateType.Message => _dvbBot.ComputeMessageAsync(update.Message),
+                UpdateType.CallbackQuery => _dvbBot.ComputeCallbackQueryAsync(update.CallbackQuery),
                 _ => throw new ArgumentOutOfRangeException()
             };
 
